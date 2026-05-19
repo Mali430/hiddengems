3c9bef803944bc7a935278b0e649ffef974a004f
Savi <deffsavi@Mohameds-MacBook-Air.local>
2026-05-19 23:04:29 +0400

    Fix all RTF files to plain text

--- src/index.js
+++ src/index.js
@@ -1,29 +1,6 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2868
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 .AppleSystemUIFontMonospaced-Regular;}
-{\colortbl;\red255\green255\blue255;\red246\green246\blue244;\red33\green33\blue31;\red133\green131\blue121;
-}
-{\*\expandedcolortbl;;\cssrgb\c97255\c97255\c96471;\cssrgb\c17255\c17255\c16471;\cssrgb\c59216\c58431\c54902;
-}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-\f0\fs18 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 import React from 'react';\cb1 \
-\pard\pardeftab720\partightenfactor0
-\cf4 \strokec4 \
-\pard\pardeftab720\partightenfactor0
-\cf2 \cb3 \strokec2    import ReactDOM from 'react-dom/client';\cb1 \
-\pard\pardeftab720\partightenfactor0
-\cf4 \strokec4 \
-\pard\pardeftab720\partightenfactor0
-\cf2 \cb3 \strokec2    import App from './App';\cb1 \
-\pard\pardeftab720\partightenfactor0
-\cf4 \strokec4 \
-\pard\pardeftab720\partightenfactor0
-\cf2 \cb3 \strokec2    const root = ReactDOM.createRoot(document.getElementById('root'));\cb1 \
-\pard\pardeftab720\partightenfactor0
-\cf4 \strokec4 \
-\pard\pardeftab720\partightenfactor0
-\cf2 \cb3 \strokec2    root.render(<App />);\cb1 \
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
